70f647eca4b14bc774002a10f1dcfc90968aa025
Oscar Jurado <oscarvmw@gmail.com>
2022-03-04 11:42:58 -0500

    Update Home

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,61 +1,34 @@
-<template>
-  <Head title="Inicio" />
-  <GuestLayout>
-    <section class="listing-container">
-      <div
-        class="listing-content flex flex-wrap items-center justify-center px-10 lg:px-20 xl:px-96 py-7"
-      >
-        <div class="w-full md:w-1/2 text-white">
-          <div class="flex flex-wrap justify-center">
-            <h1 class="w-full text-2xl font-bold">Titulo Pelicula</h1>
-            <p
-              class="w-full"
-            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dignissimos voluptates ipsa nisi quasi, iure praesentium commodi omnis minima minus. Unde quas vero nam aliquid officia temporibus explicabo modi dolorum!</p>
-          </div>
-        </div>
-        <div class="w-full md:w-1/2 text-white">
-          <div class="flex flex-wrap">
-            <div class="w-full">
-              <img
-                alt="poster"
-                class="poster md:max-w-xs mx-auto"
-                src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/miZFgV81xG324rpUknQX8dtXuBl.jpg"
-              />
-            </div>
-            <div class="mx-auto">Horarios y entradas</div>
-          </div>
-        </div>
-      </div>
-    </section>
-  </GuestLayout>
-</template>
-
-<style scoped>
-.listing-container {
-  background-position-x: center;
-  background-size: cover;
-  background-image: url("https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/1Rr5SrvHxMXHu5RjKpaMba8VTzi.jpg");
-  border-radius: 0px 0px 5px 5px;
-}
-.listing-content {
-  background: linear-gradient(
-    180deg,
-    rgba(0, 0, 0, 0) 0%,
-    rgba(0, 0, 0, 0.9) 56.25%
-  );
-}
-</style>
-
-<script>
-import GuestLayout from '@/Layouts/Guest.vue';
+<script setup>
 import { Head, Link } from '@inertiajs/inertia-vue3';
+import MainLayout from '@/Layouts/Main.vue';
+import ListingCarousel from '@/Components/Carousel.vue'
 
-export default {
-  components: {
-    GuestLayout,
-    Head,
-    Link
+const listingItems = [
+  {
+    id: 0,
+    title: 'Spiderman Sin camino a casa',
+    poster_url: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/miZFgV81xG324rpUknQX8dtXuBl.jpg',
+    synopsis: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dignissimos voluptates ipsa nisi quasi, iure praesentium commodi omnis minima minus. Unde quas vero nam aliquid officia temporibus explicabo modi dolorum!',
+    trailer_url: 'https://www.youtube.com/watch?v=r6t0czGbuGI',
+    available: 1,
+    movie_length: 140
+  },
+  {
+    id: 1,
+    title: 'The Batman',
+    poster_url: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/pOaKyhMwALpCTg07eQQu0SQCbL9.jpg',
+    synopsis: 'Cuando un asesino se dirige a la élite de Gotham con una serie de maquinaciones sádicas, un rastro de pistas crípticas envía Batman a una investigación en los bajos fondos. A medida que las pruebas comienzan a acercarse a su casa y se hace evidente la magnitud de los planes del autor, Batman debe forjar nuevas relaciones, desenmascarar al culpable y hacer justicia al abuso de poder y la corrupción que durante mucho tiempo han asolado Gotham City.',
+    trailer_url: 'https://youtu.be/L6DEcPaNdLs',
+    available: 1,
+    movie_length: 160
   }
-}
+]
 
 </script>
+
+<template>
+  <Head title="Inicio" />
+  <MainLayout>
+    <ListingCarousel :items="listingItems" />
+  </MainLayout>
+</template>
