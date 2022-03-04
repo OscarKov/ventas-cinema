7852bf2c55fc0a5003a00e91d134a8e9fd400c0f
Oscar Jurado <oscarvmw@gmail.com>
2022-03-04 12:46:54 -0500

    Update user phone field

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use phpDocumentor\Reflection\Types\Nullable;
 
 class CreateUsersTable extends Migration
 {
@@ -18,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('first_name', 45);
             $table->string('last_name', 45);
             $table->string('email')->unique();
-            $table->string('phone', 20);
+            $table->string('phone', 20)->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
