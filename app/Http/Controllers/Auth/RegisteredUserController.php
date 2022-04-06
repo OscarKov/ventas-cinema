1e925ede2e773169b15c0acd15d8616a42c04297
Oscar Jurado <oscarvmw@gmail.com>
2022-04-06 17:46:24 -0500

    Set default role for new users

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Role;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
@@ -50,6 +51,9 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
+        $role = Role::where('name', 'Cliente')->first();
+        $user->roles()->attach($role);
 
         event(new Registered($user));
 
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -6,6 +6,12 @@ const title = 'Panel de Usuario'
 
 const user = usePage().props.value.auth.user
 
+const isAdmin = () => {
+  return user.roles.some(
+    role => role.name == 'Administrador'
+  )
+}
+
 </script>
 
 <template>
@@ -14,15 +20,28 @@ const user = usePage().props.value.auth.user
   <MainLayout>
     <div class="py-12">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex">
-        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="bg-white overflow-hidden">
           <h2 class="text-lg">Que deseas hacer hoy {{ user.first_name }}?</h2>
-          <div class="p-6 bg-white border-b border-gray-200">
+          <div
+            class="p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg"
+            v-if="isAdmin()"
+          >
             <ul>
               <li>
                 <Link
                   :href="route('dashboard.movies')"
                   class="text-indigo-800"
                 >Administrar Películas</Link>
+              </li>
+            </ul>
+          </div>
+          <div class="p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
+            <ul>
+              <li>
+                <Link
+                  :href="route('home')"
+                  class="text-indigo-800"
+                >Ver Cartelera</Link>
               </li>
             </ul>
           </div>
